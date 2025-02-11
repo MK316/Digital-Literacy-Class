426c039462bae99136c092edf68af243f05f6f95
MK <99416359+MK316@users.noreply.github.com>
2025-02-11 18:26:25 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.markdown("#### Digital Literacy Class")
+st.markdown("#### 🍃 Digital Literacy Class")
 st.caption("Last updated: Feb.11, 2025")
 
 st.image("https://github.com/MK316/Digital-Literacy-Class/raw/main/images/dl-logo1.jpg", caption="©️ MK316 Digital Literacy and English Education")
