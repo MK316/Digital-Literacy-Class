a62e8b3d66c9c9e02256641481531cba9631c5dc
MK <99416359+MK316@users.noreply.github.com>
2025-02-11 21:05:27 +0900

    Create Course_Schedule.py

---
+++ pages/Course_Schedule.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.markdown("##### Weekly schedule Spring 2025")
+
+# URL of the raw markdown file on GitHub
+markdown_url = "https://raw.githubusercontent.com/MK316/Digital-Literacy-Class/refs/heads/main/pages/readme.md"
+    
+try:
+    response = requests.get(markdown_url)
+    response.raise_for_status()  # Raises an HTTPError for bad responses
+    markdown_content = response.text
+    st.markdown(markdown_content, unsafe_allow_html=True)
+except requests.exceptions.HTTPError as err:
+    st.error(f"Failed to retrieve Markdown content: {err}")
+except requests.exceptions.RequestException as e:
+    st.error(f"Request failed: {e}")
