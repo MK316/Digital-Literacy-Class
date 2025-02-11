dfe7f4d69a09517906cc31c7296190f6c7ecda75
MK <99416359+MK316@users.noreply.github.com>
2025-02-11 20:02:59 +0900

    Update ➖Part_1_Digital_Tools.py

--- pages/➖Part_1_Digital_Tools.py
+++ pages/➖Part_1_Digital_Tools.py
@@ -5,7 +5,7 @@ def main():
     st.title('Online Resources')
     
     # Create tabs
-    tab1, tab2, tab3, tab4 = st.tabs(["💻 Coding platforms", " 📌 Digital tools" ,"🍒 Customized apps","💿 Pronunciation Lessons"])
+    tab1, tab2, tab3, tab4 = st.tabs(["💻 Coding platforms", " 📌 Digital tools" ,"🍒 Customized apps"])
     
     with tab1:
         st.markdown('#### Python coding & repositories')
@@ -130,41 +130,7 @@ def main():
         st.markdown('<a href="https://mk-316-korean-english.hf.space" class="button_link" target="_blank">🎧 App 5: Loanword English Pronunciation</a>', unsafe_allow_html=True)
         st.markdown('<a href="https://mk-316-oxford5k.hf.space" class="button_link" target="_blank">🐇 App 6: Oxford 5K Vocabulary practice</a>', unsafe_allow_html=True)
 
-        
-    with tab4:
-        st.header('Explore the resources below')
-        st.write("to improve your English pronunciation skills.")
-        st.markdown("---")
-        # Dictionary of useful links and their descriptions
-        resources = {
-            "🔎 YouGlish": {
-                "url": "https://youglish.com/",
-                "description": "Use YouTube videos to practice pronunciation in context and see how words are used in real-life speeches."
-            },
-            "🔎 Minimal Pairs Practice": {
-                "url": "https://www.englishclub.com/pronunciation/minimal-pairs.htm",
-                "description": "Interactive exercises to help you master minimal pairs and improve your ability to distinguish between similar sounds."
-            },
-            "BBC Learning English": {
-                "url": "https://www.bbc.co.uk/learningenglish/english/features/pronunciation",
-                "description": "Provides a wide range of activities and video lessons to help you improve your English pronunciation."
-            },
-            "Rachel's English": {
-                "url": "https://rachelsenglish.com/",
-                "description": "Offers comprehensive videos focusing on American English pronunciation, including the nuances of phonetic sounds."
-            },
-            "English Club Pronunciation": {
-                "url": "https://www.englishclub.com/pronunciation/",
-                "description": "Features lessons and advice on different aspects of English pronunciation, suitable for all levels."
-            }
-        }
-
-        # Display links and descriptions
-        for name, info in resources.items():
-            st.markdown(f"##### {name}")
-            st.markdown(f"[Visit the site]({info['url']})")
-            st.markdown(info['description'])
-            st.write(" ")  # Add some space between entries
+    
 
 
 if __name__ == "__main__":
