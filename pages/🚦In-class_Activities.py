ecfbc64f27a7b92e75af9585652a5ec5c31574ed
MK <99416359+MK316@users.noreply.github.com>
2025-02-11 20:56:35 +0900

    Update and rename 🚦In-class_Activities.py to 🚦Projects_to_Share.py

--- pages/🚦In-class_Activities.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-tabs = st.tabs(["📈 Github IDs", "👥 Grouping", "🔊 Text-to-Speech", "⛅ Word Cloud"])
-
---
+++ pages/🚦Projects_to_Share.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+tabs = st.tabs(["👥 Project 1", "👥 Project 2", "👥 Project 3", "👥 Project 4"])
+
