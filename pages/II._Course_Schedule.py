ca50c1976fde0d5a9b3a202e57ae0fe97591229a
MK <99416359+MK316@users.noreply.github.com>
2025-02-15 21:38:02 +0900

    Update II._Course_Schedule.py

--- pages/II._Course_Schedule.py
+++ pages/II._Course_Schedule.py
@@ -3,7 +3,7 @@ import requests
 
 tabs = st.tabs(["🍐 Weekly", "🐾 Padlet"])
 
-with tabs1[1]:
+with tabs1[0]:
     # URL of the raw markdown file on GitHub
     markdown_url = "https://raw.githubusercontent.com/MK316/Digital-Literacy-Class/refs/heads/main/pages/readme.md"
         
@@ -17,11 +17,11 @@ with tabs1[1]:
     except requests.exceptions.RequestException as e:
         st.error(f"Request failed: {e}")
 
-with tabs[2]:
+with tabs[1]:
     st.header("TBA")
 
 
-with tabs[3]:
+with tabs[2]:
     st.header("🐾 Files to share: on Padlet")
     st.write("This Padlet serves as a dynamic hub for our Acoustics course. Here, you'll find additional course materials, additional reading resources, and online tools. It's also a space for sharing files and submitting assignments.")
     st.components.v1.iframe("https://padlet.com/mirankim316/DL25", width=700, height=800)
