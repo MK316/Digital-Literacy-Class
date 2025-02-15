443d5ff544204a81b1c059067b778033250a9720
MK <99416359+MK316@users.noreply.github.com>
2025-02-15 21:37:03 +0900

    Update II._Course_Schedule.py

--- pages/II._Course_Schedule.py
+++ pages/II._Course_Schedule.py
@@ -1,15 +1,27 @@
 import streamlit as st
 import requests
 
-# URL of the raw markdown file on GitHub
-markdown_url = "https://raw.githubusercontent.com/MK316/Digital-Literacy-Class/refs/heads/main/pages/readme.md"
-    
-try:
-    response = requests.get(markdown_url)
-    response.raise_for_status()  # Raises an HTTPError for bad responses
-    markdown_content = response.text
-    st.markdown(markdown_content, unsafe_allow_html=True)
-except requests.exceptions.HTTPError as err:
-    st.error(f"Failed to retrieve Markdown content: {err}")
-except requests.exceptions.RequestException as e:
-    st.error(f"Request failed: {e}")
+tabs = st.tabs(["🍐 Weekly", "🐾 Padlet"])
+
+with tabs1[1]:
+    # URL of the raw markdown file on GitHub
+    markdown_url = "https://raw.githubusercontent.com/MK316/Digital-Literacy-Class/refs/heads/main/pages/readme.md"
+        
+    try:
+        response = requests.get(markdown_url)
+        response.raise_for_status()  # Raises an HTTPError for bad responses
+        markdown_content = response.text
+        st.markdown(markdown_content, unsafe_allow_html=True)
+    except requests.exceptions.HTTPError as err:
+        st.error(f"Failed to retrieve Markdown content: {err}")
+    except requests.exceptions.RequestException as e:
+        st.error(f"Request failed: {e}")
+
+with tabs[2[:
+    st.header("TBA")
+
+
+with tabs[3]:
+    st.header("🐾 Files to share: on Padlet")
+    st.write("This Padlet serves as a dynamic hub for our Acoustics course. Here, you'll find additional course materials, additional reading resources, and online tools. It's also a space for sharing files and submitting assignments.")
+    st.components.v1.iframe("https://padlet.com/mirankim316/DL25", width=700, height=800)
