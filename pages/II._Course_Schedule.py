49963e37d2941e74bfc9d17a0bb81e3a876ac9ce
MK <99416359+MK316@users.noreply.github.com>
2025-02-23 23:03:23 +0900

    Update II._Course_Schedule.py

--- pages/II._Course_Schedule.py
+++ pages/II._Course_Schedule.py
@@ -20,5 +20,5 @@ with tabs[0]:
 
 with tabs[1]:
     st.header("🐾 Files to share: on Padlet")
-    st.write("This Padlet serves as a dynamic hub for our Acoustics course. Here, you'll find additional course materials, additional reading resources, and online tools. It's also a space for sharing files and submitting assignments.")
+    st.markdown("+ [This Padlet](https://padlet.com/mirankim316/DL25) serves as a dynamic hub for our Acoustics course. Here, you'll find additional course materials, additional reading resources, and online tools. It's also a space for sharing files and submitting assignments.")
     st.components.v1.iframe("https://padlet.com/mirankim316/DL25", width=700, height=800)
