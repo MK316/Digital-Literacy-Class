a5a7ca3d830c671848bb80ad12fc8a05216bf1eb
MK <99416359+MK316@users.noreply.github.com>
2025-02-25 14:40:12 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
 st.markdown("#### 🍃 Digital Literacy Class")
-st.caption("Last updated: Feb.25, 2025")
+st.caption('"Any sufficiently advanced technology is indistinguishable from magic." - Arthur C. Clake')
 
 st.image("https://github.com/MK316/Digital-Literacy-Class/raw/main/images/dl-logo1.jpg", caption="©️ MK316 Digital Literacy and English Education")
 
 st.markdown("---")
 st.markdown("⛺Goto [MK316 home](https://mk316.github.io)")
+st.caption("Last updated: Feb.25, 2025")
