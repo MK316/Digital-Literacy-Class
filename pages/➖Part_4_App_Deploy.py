5cfb3a02e4a4a86dd901d7515059872f09861436
MK <99416359+MK316@users.noreply.github.com>
2025-03-05 17:09:45 +0900

    Update ➖Part_4_App_Deploy.py

--- pages/➖Part_4_App_Deploy.py
+++ pages/➖Part_4_App_Deploy.py
@@ -1,1 +1,5 @@
 import streamlit as st
+
+st.write("App Deployment for Absolute Beginners")
+
+st.caption("Deploying an app means making it available for others to use online. After writing and testing your code, you can host your app on a platform like Streamlit Cloud, Hugging Face Spaces, or GitHub Pages. These platforms take care of the setup, so you don't need advanced server knowledge. For beginners, Streamlit is a great choice because it allows you to deploy a Python app with just a few clicks. You upload your code to GitHub, connect it to Streamlit Cloud, and your app goes live with a shareable link. This makes it easy to create and share interactive applications without complex configurations.")
