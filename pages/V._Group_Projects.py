e32c714ffd5407ff219cee7322aac177b7267f8f
MK <99416359+MK316@users.noreply.github.com>
2025-03-10 11:22:16 +0900

    Update V._Group_Projects.py

--- pages/V._Group_Projects.py
+++ pages/V._Group_Projects.py
@@ -1,24 +1,26 @@
 import streamlit as st
 
 # Create three tabs
-tabs = st.tabs(["📌 Instructions", "💻 GitHub IDs", "🖼️ Project Gallery"])
+tabs = st.tabs(["📌 Project list", "💻 GitHub IDs", "🖼️ Project Gallery"])
 
 # First tab: Instructions
 with tabs[0]:
-    st.markdown("## 📌 Project Instructions")
+    st.markdown("### 📌 Project List")
     st.write("""
     Welcome to the project portal! This page will provide details of each project when scheduled:
     """)
 
+    st.markdown("#### No project is assigned yet. (as of 2025. 03. 09)")
+
 # Second tab: GitHub IDs
 with tabs[1]:
-    st.markdown("## 💻 GitHub IDs for Group Projects")
+    st.markdown("### 💻 GitHub IDs for Group Projects")
     st.page_link("https://docs.google.com/spreadsheets/d/1z2uYvH-foo3BZ6a4_T80TK7HOQbIJIYIUe5SWOEaGyk/edit?usp=sharing", label="Make your own account and share your IDs here", icon="➡️")
   
 
 # Third tab: Project Gallery
 with tabs[2]:
-    st.markdown("## 🖼️ Project Gallery")
+    st.markdown("### 🖼️ Project Gallery")
     st.write("Once the projects are submitted, they will be displayed below:")
 
 # Footer
