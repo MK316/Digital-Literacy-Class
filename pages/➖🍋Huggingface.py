208ceeca103f02b63e356b43bdea4700ede7c5e2
MK <99416359+MK316@users.noreply.github.com>
2025-03-26 00:36:27 +0900

    Update ➖🍋Huggingface.py

--- pages/➖🍋Huggingface.py
+++ pages/➖🍋Huggingface.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
-st.caption("About Huggingface")
+# Adding a caption or description with a link using markdown
+st.markdown("""
+### About Hugging Face
+Hugging Face is a technology company that specializes in Natural Language Processing (NLP) technology. For more information, visit [Hugging Face](https://huggingface.co).
+""")
+
+st.markdown("---")
+st.markdown("#### To do")
+st.write("Open your account on Huggingface. You may consider the same ID with Github so that you don't forget the ID :-)")
