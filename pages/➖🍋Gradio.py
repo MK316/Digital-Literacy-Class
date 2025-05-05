49c2be176ff7c98fe5dc29af8df9d9dbdc56a4f8
MK <99416359+MK316@users.noreply.github.com>
2025-05-05 20:45:42 +0900

    Update ➖🍋Gradio.py

--- pages/➖🍋Gradio.py
+++ pages/➖🍋Gradio.py
@@ -3,4 +3,6 @@ import streamlit as st
 st.caption("About Gradio")
 st.write("Gradio is a free and easy-to-use Python library that lets you build interactive web apps for machine learning or data tools with just a few lines of code. You can create buttons, text boxes, image displays, or audio features that help users try your models or programs right in the browser—without needing any web design skills.")
 
-st.markdown("Go to [Gradio](https://www.gradio.app/)")
+st.markdown("💦 Go to [Gradio](https://www.gradio.app/)")
+
+st.markdown("💦 Sample app using Gradio [Story to sentences](https://mk-316-story-to-sentence.hf.space/)")
