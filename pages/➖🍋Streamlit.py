62bdda3399a35335b15ea12a0fb7edc103f7ebeb
MK <99416359+MK316@users.noreply.github.com>
2025-05-07 11:36:54 +0900

    Update ➖🍋Streamlit.py

--- pages/➖🍋Streamlit.py
+++ pages/➖🍋Streamlit.py
@@ -1,5 +1,12 @@
 import streamlit as st
 
-st.caption("About Streamlit")
+st.markdown("### About Streamlit")
 
+st.write("Streamlit is a simple and powerful Python library that lets you turn your code into interactive web apps for data analysis, machine learning, or education—without needing to know HTML or JavaScript. With just a few lines of code, you can add sliders, buttons, charts, and file upload tools to make your project easy to use and share in a browser.")
+
+st.markdown("""
+💦 Go to [Streamlit](https://share.streamlit.io/)  
+💦 Go to [Streamlit practice](https://github.com/MK316/Digital-Literacy-Class/blob/main/pages/streamlit.md)  
+
+""") 
 
