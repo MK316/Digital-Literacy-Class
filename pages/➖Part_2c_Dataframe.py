734b9cf654726acfe68580ed8e536fa55a371f8d
MK <99416359+MK316@users.noreply.github.com>
2025-05-12 09:48:33 +0900

    Update ➖Part_2c_Dataframe.py

--- pages/➖Part_2c_Dataframe.py
+++ pages/➖Part_2c_Dataframe.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
 st.markdown("### Dataframe using Pandas")
-
-st.markdown("[Worksheet 1]()")
+st.write("""📌 Description:
+This section introduces how to create and work with a DataFrame using the pandas library. A DataFrame is a table-like data structure in Python that allows you to organize, view, and analyze data easily — similar to an Excel spreadsheet.
+""")
+st.markdown("[Worksheet 1](https://github.com/MK316/Digital-Literacy-Class/blob/main/pages/dataframe.md)")
