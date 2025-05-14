4054a6dd6defb28d12b6a6a12bc53ee61c27c053
MK <99416359+MK316@users.noreply.github.com>
2025-05-14 12:11:42 +0900

    Update ➖Part_2c_Dataframe.py

--- pages/➖Part_2c_Dataframe.py
+++ pages/➖Part_2c_Dataframe.py
@@ -5,3 +5,4 @@ st.write("""📌 Description:
 This section introduces how to create and work with a DataFrame using the pandas library. A DataFrame is a table-like data structure in Python that allows you to organize, view, and analyze data easily — similar to an Excel spreadsheet.
 """)
 st.markdown("[Worksheet 1](https://github.com/MK316/Digital-Literacy-Class/blob/main/pages/dataframe.md)")
+st.markdown("[Worksheet 2](https://github.com/MK316/Digital-Literacy-Class/blob/main/pages/dataframe2.md): app codes with frequency data")
