7112f1e61d191b7d0b0c189f316815069cbc29f2
MK <99416359+MK316@users.noreply.github.com>
2025-06-02 13:29:11 +0900

    Update 🚦Final_Project_Gallery.py

--- pages/🚦Final_Project_Gallery.py
+++ pages/🚦Final_Project_Gallery.py
@@ -40,7 +40,7 @@ with tab2:
     st.image(image_url, caption="Spring 2025 Digital Literacy & English Education", width=200)
     
     st.markdown("""
-    + Group 1 Digital Classroom: [Click to Enter](https://g01final.streamlit.app/)
+    + Group 1 Digital Classroom (Streamlit app): [Click to Enter](https://g01final.streamlit.app/)
     + Github files to view: [link](https://github.com/yunju05/G02Final/raw/main/README.md)
     """)
 # Tab 3: Project 2 (G2)
@@ -51,7 +51,7 @@ with tab3:
     st.image(image_url, caption="Spring 2025 Digital Literacy & English Education", width=200)
     
     st.markdown("""
-    + Group 2 Digital Classroom: [Click to Enter](https://g02final.streamlit.app/)
+    + Group 2 Digital Classroom (Streamlit app): [Click to Enter](https://g02final.streamlit.app/)
     + Github files to view: [link](https://github.com/KY7437/G01Final/raw/main/README.md)
     """)
 # Tab 4: Project 3 (G3)
@@ -62,6 +62,6 @@ with tab4:
     st.image(image_url, caption="Spring 2025 Digital Literacy & English Education", width=200)
     
     st.markdown("""
-    + Group 3 Digital Classroom: [Click to Enter](https://g03final.streamlit.app/)
+    + Group 3 Digital Classroom (Streamlit app): [Click to Enter](https://g03final.streamlit.app/)
     + Github files to view: [link](https://github.com/JW-1211/G03Final/raw/main/README.md)
     """)
