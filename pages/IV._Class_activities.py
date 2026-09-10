5a6d9bbd5ccdcfb97df8aca15016ff4bab03ff3c
MK <99416359+MK316@users.noreply.github.com>
2026-09-10 12:03:29 +0900

    Update IV._Class_activities.py

--- pages/IV._Class_activities.py
+++ pages/IV._Class_activities.py
@@ -15,8 +15,8 @@ PDF_FILES = {
     "1. HTML 가이드": "pages/data/week02/files/01-HTML-guide.pdf",
     "2. 과업 안내문": "pages/data/week02/files/02-task-guide.pdf",
     "3. Task 01": "pages/pages/data/week02/files/Task_01.pdf",
-    "4. Task 02": "data/week02/files/Task_02.pdf",
-    "5. Task 03": "data/week02/files/Task_03.pdf",
+    "4. Task 02": "pages/data/week02/files/Task_02.pdf",
+    "5. Task 03": "pages/data/week02/files/Task_03.pdf",
 }
 
 # -----------------------------
