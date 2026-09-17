7a550448e755e984fb87d211614649eb7b0fe9d0
MK <99416359+MK316@users.noreply.github.com>
2026-09-17 17:25:11 +0900

    Create 99📮_Message_Board.py

---
+++ pages/99📮_Message_Board.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.set_page_config(page_title="Class Padlet Wall", layout="wide")
+
+st.header("🐾 Sharing for classroom activities")
+st.markdown("""
+    This Padlet serves as a dynamic hub for Q & As regarding the course. Personal inquiries can be directred via E-mail at _mirankim@gnu.ac.kr_  
+    """)
+st.components.v1.iframe("https://padlet.com/mirankim316/F26_dlee", width=800, height=600)
